25e9db6df572417b666f4697b15e226c5b1a7c86
PRITIBARDHAN JHA <56965172+Pritivardhan@users.noreply.github.com>
2021-04-25 14:29:13 +0530

    Add files via upload

--- diameter_1.cpp
+++ diameter_1.cpp
@@ -79,6 +79,40 @@ int daim(vector<int> adj[],int s,int &len){
 	}
 	return farthest;
 }
+void farthest_all(vector<int> adj[],int s,int &len,int dt[]){
+	queue<pair<int,int> > q;
+	q.push({s,0});
+	vis[s]=1;
+	int farthest=s;
+	 len=0;
+	while(!q.empty()){
+		int siz=q.size();
+		//cout<<siz<<"\n";
+		for(int i=1;i<=siz;i++){
+
+			auto temp=q.front();
+			q.pop();
+			int l=temp.S;
+			int n=temp.F;
+			//cout<<n<<" "<<l<<"\n";
+			//cout<<l<<" ";
+			dt[n]=max(dt[n],l);
+			/*if(l>len){
+				len=l;
+				farthest=n;
+
+
+			}*/
+			for(auto it:adj[n]){
+				if(vis[it]==0){
+				q.push({it,l+1});
+				vis[it]=1;
+			}
+			}
+		}
+	}
+	//return farthest;
+}
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -97,14 +131,25 @@ int main() {
     
     memset(vis,0,sizeof(vis));
     int len=0;
-   int u=daim(adj,1,len); 
-   // cout<<u<<" "<<len<<"\n";
+   int u=daim(adj,1,len);  // u is one of the farthest node from root 
+   
      memset(vis,0,sizeof(vis));
      len=0;
 
-     int v=daim(adj,u,len);
-     cout<<len<<"\n";
+     int v=daim(adj,u,len); // v is farthest from u i.e u and v is farthest among all pair of nodes;
 
+    // cout<<len<<"\n";
+     int dt[t+1]={0};
+     len=0;
+      memset(vis,0,sizeof(vis));
+     farthest_all(adj,u,len,dt);   // for every node find a distance from u i.e is one of the farthest.
+      len=0;
+       memset(vis,0,sizeof(vis));
+     farthest_all(adj,v,len,dt); // for each node find distance from v . which ever distance is gretest assign to each node
+    // cout<<"\n";
+     for(int i=1;i<=t;i++)
+     	cout<<dt[i]<<" ";
+     cout<<"\n";
 
 	return 0;
 }
